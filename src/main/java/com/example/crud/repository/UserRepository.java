ec8a781aaf067a6a1e59828ac817f63c3edf04d9
Hyeongchan Shin <intshc@gmail.com>
2023-06-11 18:16:02 +0900

    RepositoryRestResourse 추가(/api/도메인/search)

--- src/main/java/com/example/crud/repository/UserRepository.java
+++ src/main/java/com/example/crud/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.crud.repository;
 import com.example.crud.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource
 public interface UserRepository extends CrudRepository<User, Long> {
     //Optional로 예외처리 가능하다
     Optional<User> findByEmailAndPassword(String email, String password);
